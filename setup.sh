854119520014888b0509a9f15967c781141bae4e
Simón Pedro <simon.pedro.g@gmail.com>
2025-04-09 01:05:07 -0600

    doc setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
+
+# from 0:
 uv init --python 3.11
 uv add dm-meltingpot vmas
 
-cd thirdparty
-git clone BenchMARL
+# using current pyproject.toml
+uv sync
 
+mkdir thirdparty
+cd thirdparty
+git clone https://github.com/facebookresearch/BenchMARL.git
+cd ..
 uv pip install -e thirdparty/BenchMARL
 
 git clone https://github.com/pytorch/tensordict.git
-
-cd thirdparty/tensordict
+cd tensordict
 git checkout 3a0a8bb
+
 cd ../..
 uv pip install -e thirdparty/tensordict
 
